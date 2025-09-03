dfd9f6463c884b0faf0988486fd0cab5cbb439f9
vooft <52610+vooft@users.noreply.github.com>
2025-09-03 10:47:03 +0400

    Add publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,3 +16,27 @@ allprojects {
         mavenCentral()
     }
 }
+
+tasks.register("spektorPublishToMavenCentral") {
+    dependsOn(
+        subprojects.mapNotNull { it.tasks.findByName("publishAndReleaseToMavenCentral") } +
+            gradle.includedBuilds.map { it.task(":spektorPublishToMavenCentral") } +
+            listOfNotNull(tasks.findByName("publishAndReleaseToMavenCentral"))
+    )
+}
+
+tasks.register("spektorPublishToMavenLocal") {
+    dependsOn(
+        subprojects.mapNotNull { it.tasks.findByName("publishToMavenLocal") } +
+            gradle.includedBuilds.map { it.task(":spektorPublishToMavenLocal") } +
+            listOfNotNull(tasks.findByName("publishToMavenLocal"))
+    )
+}
+
+tasks.register("spektorPublishPlugins") {
+    dependsOn(
+        subprojects.mapNotNull { it.tasks.findByName("publishPlugins") } +
+            gradle.includedBuilds.map { it.task(":spektorPublishPlugins") } +
+            listOfNotNull(tasks.findByName("publishPlugins"))
+    )
+}
--- spektor-gradle-plugin/build.gradle.kts
+++ spektor-gradle-plugin/build.gradle.kts
@@ -34,3 +34,27 @@ dependencies {
 
     implementation(libs.kotlin.gradle.plugin.api)
 }
+
+tasks.register("spektorPublishToMavenCentral") {
+    dependsOn(
+        subprojects.mapNotNull { it.tasks.findByName("publishAndReleaseToMavenCentral") } +
+                gradle.includedBuilds.map { it.task(":spektorPublishToMavenCentral") } +
+                listOfNotNull(tasks.findByName("publishAndReleaseToMavenCentral"))
+    )
+}
+
+tasks.register("spektorPublishToMavenLocal") {
+    dependsOn(
+        subprojects.mapNotNull { it.tasks.findByName("publishToMavenLocal") } +
+                gradle.includedBuilds.map { it.task(":spektorPublishToMavenLocal") } +
+                listOfNotNull(tasks.findByName("publishToMavenLocal"))
+    )
+}
+
+tasks.register("spektorPublishPlugins") {
+    dependsOn(
+        subprojects.mapNotNull { it.tasks.findByName("publishPlugins") } +
+                gradle.includedBuilds.map { it.task(":spektorPublishPlugins") } +
+                listOfNotNull(tasks.findByName("publishPlugins"))
+    )
+}

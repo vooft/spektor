dbe85f155f95ecf6d43a19cc2ec3a3a0bbf9dee6
vooft <52610+vooft@users.noreply.github.com>
2026-03-09 15:24:38 +0400

    fix build

--- spektor-gradle-plugin/src/main/kotlin/io/github/vooft/spektor/gradle/SpektorGenerateTask.kt
+++ spektor-gradle-plugin/src/main/kotlin/io/github/vooft/spektor/gradle/SpektorGenerateTask.kt
@@ -10,17 +10,22 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.provider.MapProperty
 import org.gradle.api.provider.Property
+import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputDirectory
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.OutputDirectory
+import org.gradle.api.tasks.PathSensitive
+import org.gradle.api.tasks.PathSensitivity
 import org.gradle.api.tasks.TaskAction
 
+@CacheableTask
 abstract class SpektorGenerateTask : DefaultTask() {
 
     @get:OutputDirectory
     abstract val outputPath: DirectoryProperty
 
+    @get:PathSensitive(PathSensitivity.ABSOLUTE)
     @get:InputDirectory
     abstract val specRoot: DirectoryProperty
 
